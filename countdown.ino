7f9bccd33c4a8854bcc4fe28b2f3b974efa635ee
Soulier Antoine <soulier.antoine@live.fr>
2017-04-05 11:54:21 +0200

    Added 7th TP : countdown

---
+++ countdown.ino
@@ -0,0 +1,173 @@
+#define SEGMENT_ON HIGH
+#define SEGMENT_OFF LOW
+
+const int buttonPin = 2;
+
+const int segDP =  4;
+const int segC =  5;
+const int segB =  6;
+const int segA =  7;
+const int segF =  8;
+const int segG =  9;
+const int segE =  10;
+const int segD =  11;
+
+int previousButtonState = 0;
+int buttonState = 0;
+int cpt = 9;
+
+void setup() {
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+  pinMode(segDP, OUTPUT);
+  
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  // Pressed
+  if (buttonState == HIGH) {
+    if (buttonState != previousButtonState) {
+      displayNumber(cpt);
+      cpt--;
+      if (cpt == -1) 
+        cpt = 9;
+    }
+    
+    previousButtonState = buttonState;
+    
+  // Released
+  } else {
+    previousButtonState = buttonState;
+  }
+}
+
+void allLedDown() {
+  digitalWrite(segDP, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+
+void displayNumber(int number) {
+  Serial.print("NUMBER: ");
+  Serial.println(number);
+  
+  switch (number) {
+    case 0:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+    case 1:
+      digitalWrite(segA, SEGMENT_OFF);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+    case 2:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_OFF);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+
+    case 3:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+  
+    case 4:
+      digitalWrite(segA, SEGMENT_OFF);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+  
+    case 5:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_OFF);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+
+    case 6:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_OFF);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+
+    case 7:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+    case 8:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+
+    case 9:
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+    break;
+    
+    default:
+      allLedDown();
+    break;
+  }
+}
+
