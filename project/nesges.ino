6e6da22a3de9768e3afc5b2a9073ca58a0b1a2ff
Soulier Antoine <soulier.antoine@live.fr>
2017-04-06 18:31:48 +0200

    Added TCPServer and updated project

--- project/nesges.ino
+++ project/nesges.ino
@@ -28,35 +28,59 @@ void setup()
 }
 
 void loop() {
-  /* if (Serial3.available() && isReady) {
-    Serial3.print("AT+CIPSTART=4,\"TCP\",\"192.168.30.42\",\"6789\"\r\n");
-    delay(1000);
-    
-    Serial3.print("AT+CIPSEND=4, 10\r\n");
-    delay(500);
-    
-    // Serial3.print(request);
-    // delay(3000);
-    
-    // on ferme la connexion
-    Serial3.write("AT+CIPCLOSE=4");
-  } */
+  // À faire lors de la 1ère connexion à un réseau étranger :
+  // AT+CWJAP="ESGI","Reseau-GES"  
 
-  while (Serial.available() > 0) {
+  // Nb de connexions simultanées :
+  // AT+CIPMUX=1
+
+  // Lancement de la connexion TCP :
+  // AT+CIPSTART=1,"TCP","192.168.30.42",6789
+  
+  // Envoi de 10 bytes sur la connexion avec l'id 1 :
+  // AT+CIPSEND=1,10
+
+  // Ferme la connexion avec l'id 1
+  // AT+CIPCLOSE=1
+
+  // Ping Google :
+  // AT+PING="www.google.fr"
+  
+  if (Serial3.available() && isReady) {
+    Serial3.print("AT+CIPMUX=1\r\n");
+    delay(50);
+    
+    Serial3.print("AT+CIPSTART=1,\"TCP\",\"192.168.30.42\",6789\r\n");
+    delay(100);
+    
+    Serial3.print("AT+CIPSEND=1, 6\r\n");
+    delay(50);
+    
+    Serial3.print("ping\r\n");
+    delay(50);
+    
+    Serial3.print("AT+CIPCLOSE=1\r\n");
+    delay(50);
+  }
+
+  /* while (Serial.available() > 0) {
     char a = Serial.read();
     Serial3.write(a);
-  }
+  } */
 }
 
 // Is triggered whenever the ESP sends back something
 void serialEvent3() {
-  /* if (Serial3.available() > 0) {
+  if (Serial3.available() > 0) {
       if (!isReady && Serial3.find("ready")) {
         isReady = true; 
+      } else {
+        char a = Serial3.read();
+        Serial.write(a);        
       }
-  } */
-  while (Serial3.available() > 0) {
+  }
+  /* while (Serial3.available() > 0) {
     char a = Serial3.read();
     Serial.write(a);
-  }
+  } */
 }
